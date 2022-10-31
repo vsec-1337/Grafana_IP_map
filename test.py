a5adddcc4c9959598fd16fd82d22e84888e30014
blazer618 <88243997+blazer618@users.noreply.github.com>
2022-10-31 12:07:05 +0100

    Add files via upload
    Read in live a .log file

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import ipaddress
 import re
+import time, os
 
-#[+] ----- FUNCTIONS BLOCK ----- [+]
+#[+] ----- IP FUNCTIONS BLOCK ----- [+]
 ### IP2Location database converter ###
 def long2DotIP(ipnum):
     return str(int(ipnum / 16777216) % 256) + "." + str(int(ipnum / 65536) % 256) + "." + str(int(ipnum / 256) % 256) + "." + str(ipnum % 256)
@@ -13,7 +14,7 @@ def dot2LongIP(ip):
 
 
 #[+] ----- CODE BLOCK ----- [+]
-### Convert all database long 2 readable IPs
+### Convert all database 'long' 2 'readable' IPs
 filestream_path = 'C:/temp/test_ip.txt'
 answer_path = 'C:/temp/answer_ip.txt'
 with open(filestream_path, 'r') as filestream:
@@ -31,11 +32,23 @@ with open(filestream_path, 'r') as filestream:
 # /// TO COMPLETE ///
 ### ###
 
-### Extract latitude / longitude from IP field
-# /// TO COMPLETE ///
-### ###
+# Read in live nginx log file
+file = open('/var/log/nginx/access.log', mode='r')
+i = 1
+while 1:
+#       where = file.tell()
+        line = file.readline()
+        if not line:
+                time.sleep(0.5)
+                #file.seek(where)
+        else:
+                print(line)
+                #Block to add retrieve IP and add in database a value
+                i = i + 1
+                print(i)
 
-### Get timestamp
+
+### Extract latitude / longitude from IP field
 # /// TO COMPLETE ///
 ### ###
 
